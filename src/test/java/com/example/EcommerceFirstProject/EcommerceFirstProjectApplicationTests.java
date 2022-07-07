ca0b197136c660ffb94b620e19a5597102105529
Thaparoshan1477 <thaparoshan629@gmail.com>
2022-07-07 14:20:52 -0700

    hey this my code

---
+++ src/test/java/com/example/EcommerceFirstProject/EcommerceFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EcommerceFirstProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
